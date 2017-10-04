74cccaa9cce85ae3ee2b6fe2a43136bcdd587098
Brian Schott <brian@bronxsoftware.org>
2017-10-04 07:18:37 -0400

    Create Team.java

---
+++ Team.java
@@ -0,0 +1,32 @@
+public class Team {
+  
+  /* Parameters */
+  
+  // Create the following parameters
+  // A String parameter for the Team's name
+  // A int parameter for the Team's current score
+  // An int parameter for the team's rank
+  // A double parameter for the team's free-throw percent (between 0.0 and 1.0)
+  
+  /* Constructor */
+  
+  //Write your first constructor below
+  //The constructor should have the team's name as its parameter
+  //The constructor should set the team's name to the parameter's value
+  //The constructor should also set the current score to zero, the rank to 1, and the free throw percentage to .5
+  
+  //Write your second constructor below
+  //The constructor should have the team's name, the team's rank, and team's free-throw percent as parameters
+  //The constructor should set all of the class's properties to the values of the parameters
+  //It should also set the team's current score to zero
+  
+  /* Methods */
+  //Write get and set methods for each of the 4 default parameters (that's 8 methods)
+  
+  //Write a method called scoreTwo() that has no parameters and void return type
+  //The method will increase the team's score by 2
+  
+  //Write a method called scoreThree() that has no parameters and void return type
+  //The method will increase the team's score by 3
+  
+}

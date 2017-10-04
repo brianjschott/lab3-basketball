3736da379596603223344ee279dae7a1d7441c6a
Brian Schott <brian@bronxsoftware.org>
2017-10-04 14:40:47 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,6 +2,29 @@ public class Game {
   
   public static void main(String[] args) {
     
-    //Create two Team objects here 
+    /* Create two Team objects here */
+    //Use the first constructor for the first team object
+    //Use the second constructor for the second team object
+    //Use whatever parameters you want for each team as long as they are valid
     
-    //Use the get methods to 
+    /* Print the Initial Scores */
+    //Use the get methods to print out all the parameters for each object you created
+    //Be sure to convert the free throw decimal into a percentage!
+    //See below for an example
+    /* Team 1 Name: <name>    Score: <score>  Rank: <rank>  Free Throw %: 50.0%
+       Team 2 Name: <name>    Score: <score>  Rank: <rank>  Free Throw %: 25.0%   */
+    
+    
+    /* Play the Game */
+    //Use the score2 and score3 methods as many times as you wish for each team
+    //Be sure to use each method at least once for each object
+    
+    
+    
+    /* Print Final Scores */
+    //Use the get methods once again to print out all the parameters for each object created
+    //You may re-use the existing code above by copy and paste, OR ...
+    //20 point bonus: Create a printScores() method that does this for you
+    
+  }
+}
